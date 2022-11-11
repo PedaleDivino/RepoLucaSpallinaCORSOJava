9f09d80ff731c12153519727b30395661f3a7747
Pedale Divino <117976428+PedaleDivino@users.noreply.github.com>
2022-11-11 10:48:20 +0100

    11 / 11 / 2022 Luca Spallina mostarda4
    mostarda4

---
+++ mostarda4.java
@@ -0,0 +1,26 @@
+public class mostarda4 {
+    public static void main(String[] args){
+
+        int carattere = 'b';                       // inizializzo variabili
+        double nVirgola = 123.34f;                  
+        int intero = 1;                             // fine inizializzazione variabili
+
+        System.out.println("le variabili sono: b" + "\n il quale n in ASCII e': " + carattere + " \n " + nVirgola + " \n " + intero );
+        System.out.println();
+
+        double somma = (carattere+nVirgola+intero)/3;   // formula
+        
+        
+        boolean stBool = true;                      // inizializzo boolean
+        boolean ndBool = false;
+
+        if(ndBool != stBool){                       // condizione per la quale rendo ndBoo da FALSE a TRUE e stampa EVVIVA
+        ndBool = true;
+        System.out.println(somma + " Evviva");
+        }
+        else                                        // se non funziona stampa "hai sbagliato"
+        System.out.println("hai sbagliato bruh ");
+    }
+
+
+}
