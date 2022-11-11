1ea4baf3cea8c2f400e73ada300ab3b072d29681
Pedale Divino <117976428+PedaleDivino@users.noreply.github.com>
2022-11-11 14:01:21 +0100

    mostarda5 1.0 (blocco if,else)
    11/11/2022 Luca Spallina

---
+++ mostarda5.java
@@ -0,0 +1,17 @@
+public class mostarda5 {
+    public static void main(String[] args){
+        
+        String a = "ciao";
+        boolean b = true;
+
+        if(b == true){
+
+            System.out.println(a);
+
+        }
+        else(){
+            System.out.println("hai sbagliato ");
+        }
+
+    }
+}
